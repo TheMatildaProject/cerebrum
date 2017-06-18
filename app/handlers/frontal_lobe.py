372b78834bf818c0a5ee18b58fb98c7740d07d51
Eduardo Leoni <eduardoleoni@otlook.com>
2017-06-18 14:12:42 +1200

    Remove some of the concerns from it and mvoe it to the right micro-service

--- app/handlers/frontal_lobe.py
+++ app/handlers/frontal_lobe.py
@@ -8,12 +8,7 @@ from app.intents.chit_chat import ChitChat
 from app.intents.chit_chat_two import ChitChatTwo
 from app.intents.play_song import PlaySong
 from app.intents.todays_weather import TodaysWeather
-from pygame import mixer
-import base64
-import io
 import json
-import os
-import sys
 
 class FrontalLobe(object):
     def handleResponse(self, message: dict):
@@ -38,14 +33,6 @@ class FrontalLobe(object):
         return intentClass.handle();
     
     def playResponse(self, audio):
-        sound = bytes(audio, "utf-8")
-        sound = base64.b64decode(sound)
-
-        f = open("temp.ogg",'wb')
-        f.write(sound)
-        os.fsync(f)
-        f.close()
-
-        mixer.init()
-        mixer.music.load('temp.ogg')
-        mixer.music.play()
+        return true;
+        # should send back to the queue where vocal cord will poll from
+        
